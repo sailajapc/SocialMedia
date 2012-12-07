3423b574392f4731768cfca7dd188b400c88d0f8
paradigm-creatives <info@paradigmcreatives.com>
2012-12-07 17:36:46 +0530

    Initial Commit

---
+++ SocialMedia/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SocialMedia
+//
+//  Created by Paradigm on 12/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)connectFaceBook:(id)sender;
+- (IBAction)connectTwitter:(id)sender;
+
+@end
